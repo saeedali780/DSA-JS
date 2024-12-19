b1d656905977c2f822136a2b809429bfe1837a6e
Saeed Ali <saeedaliofficial77@gmail.com>
2024-12-19 20:34:59 +0500

    Array Traversing & Accessing

---
+++ app.js
@@ -0,0 +1,14 @@
+
+//==ARRAY TRAVERSSING
+let data = [12,3,45,67,56,3];
+
+for(let i = 0; i<data.length; i++){
+    console.log(data[i]);  
+}
+
+//ARRAY ACCESSING
+let x = data[3];
+console.log(x);
+
+
+
