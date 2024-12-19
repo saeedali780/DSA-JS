d95b660787b6fcf8957d088d6fd3bd31960b9dbf
Saeed Ali <saeedaliofficial77@gmail.com>
2024-12-19 20:52:20 +0500

    Accessing Array Element from Input

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 
-//==ARRAY TRAVERSING
-let data = [12,3,45,67,56,3];
+//====ARRAY TRAVERSING====
+// let data = [12,3,45,67,56,3];
 
-for(let i = 0; i<data.length; i++){
-    console.log(data[i]);  
-}
+// for(let i = 0; i<data.length; i++){
+//     // console.log(data[i]);  
+// }
 
-//ARRAY ACCESING
-let x = data[3];
-console.log(x);
+//====ARRAY ACCESING====
+// let x = data[3];
+// // console.log(x);
 
 
+//====ARRAY ELEMENT ACCESSING FROM INPOUT====
+
+// const btn = document.getElementById("btn");
+
+// function getElement() {
+// let el = document.getElementById("element").value;
+// if (el<data.length) {
+//     console.log(data[el]);
+    
+// }else{
+//     alert("enter valid value")
+// }
+
+// };
+
+// btn.addEventListener("click",getElement);
 
