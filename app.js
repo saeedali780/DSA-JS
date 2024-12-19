6d29bee6fa343ba309a67c6221583a0aef1148c0
Saeed Ali <saeedaliofficial77@gmail.com>
2024-12-19 20:57:50 +0500

    Array accessing from input with validations

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
 
-//====ARRAY TRAVERSING====
+// ====ARRAY TRAVERSING====
 // let data = [12,3,45,67,56,3];
 
 // for(let i = 0; i<data.length; i++){
 //     // console.log(data[i]);  
 // }
 
-//====ARRAY ACCESING====
+// ====ARRAY ACCESING====
 // let x = data[3];
 // // console.log(x);
 
 
-//====ARRAY ELEMENT ACCESSING FROM INPOUT====
+// ====ARRAY ELEMENT ACCESSING FROM INPOUT====
 
-// const btn = document.getElementById("btn");
+const btn = document.getElementById("btn");
 
-// function getElement() {
-// let el = document.getElementById("element").value;
-// if (el<data.length) {
-//     console.log(data[el]);
+function getElement() {
+let el = document.getElementById("element").value;
+parseInt(el)
+if (el<data.length) {
+    console.log(data[el]);
     
-// }else{
-//     alert("enter valid value")
-// }
+}else{
+    alert("enter valid value")
+}
 
-// };
+};
 
-// btn.addEventListener("click",getElement);
+btn.addEventListener("click",getElement);
 
