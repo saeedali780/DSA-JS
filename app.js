5c7646f601d919ef41692c1a42f428c3490822a5
Saeed Ali <saeedaliofficial77@gmail.com>
2024-12-19 21:01:11 +0500

    minus intergers validations handled

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 // ====ARRAY TRAVERSING====
-// let data = [12,3,45,67,56,3];
+let data = [12,3,45,67,56,39];
 
 // for(let i = 0; i<data.length; i++){
 //     // console.log(data[i]);  
@@ -18,7 +18,7 @@ const btn = document.getElementById("btn");
 function getElement() {
 let el = document.getElementById("element").value;
 parseInt(el)
-if (el<data.length) {
+if (el<data.length && el<=0) {
     console.log(data[el]);
     
 }else{
